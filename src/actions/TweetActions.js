1de45876a300453232cbf3d824253e3d17e986d1
Anton Stepan <antonstepan@antonstepan-macbookpro.roam.corp.google.com>
2017-05-15 22:16:35 +0100

    purge purge

--- src/actions/TweetActions.js
+++
@@ -1,8 +0,0 @@
-import * as types from '../constants/ActionTypes';
-
-export function addTweet(value) {
-  return {
-    type: types.ADD_TWEET,
-    value
-  };
-}
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as EntityList } from './EntityList';
-export { default as ListItem } from './ListItem';
-export { default as TermFrequencyList } from './TermFrequencyList';
